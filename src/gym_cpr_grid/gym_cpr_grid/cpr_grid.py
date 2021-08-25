fix social outcome metrics computation

src/gym_cpr_grid/gym_cpr_grid/cpr_grid.py
@@ -300,9 +300,6 @@ class CPRGridEnv(MultiAgentEnv, gym.Env):
             infos[agent_handle]["gifting"] = agent_handle in gifting_agents
             infos[agent_handle]["gifted"] = agent_handle in gifted_agents
 
-        # Add social outcome metrics to info dict
-        infos["__all__"] = self.get_social_outcome_metrics()
-
         # Check if we reached end of episode
         dones["__all__"] = False
         self.elapsed_steps += 1
@@ -317,6 +314,9 @@ class CPRGridEnv(MultiAgentEnv, gym.Env):
         for agent_handle in range(self.n_agents):
             observations[agent_handle] = self._get_observation(agent_handle)
             self.rewards_history[agent_handle].append(rewards[agent_handle])
+
+        # Add social outcome metrics to info dict
+        infos["__all__"] = self.get_social_outcome_metrics()
 
         # Respawn resources
         self._respawn_resources()
@@ -611,15 +611,15 @@ class CPRGridEnv(MultiAgentEnv, gym.Env):
         of sum of rewards
         """
         returns = self._get_returns()
-        return np.sum(returns) / self.elapsed_steps
+        return np.mean(returns)
 
-    def equality_metric(self, eps=1e-6):
+    def equality_metric(self):
         """
         The Equality metric (E) is defined using the Gini coefficient
         """
         returns = self._get_returns()
         numerator = np.sum([abs(ri - rj) for ri in returns for rj in returns])
-        return 1 - (numerator / (2 * len(returns) * np.sum(returns) + eps))
+        return 1 - (numerator / (2 * self.n_agents * np.sum(returns) + 1e-6))
 
     def sustainability_metric(self):
         """
@@ -630,8 +630,9 @@ class CPRGridEnv(MultiAgentEnv, gym.Env):
         for agent_handle in range(self.n_agents):
             rewards = self.rewards_history[agent_handle]
             ti = np.argwhere(np.array(rewards) > 0)
-            times.append(self._max_episode_steps if len(ti) == 0 else np.mean(ti))
-        return np.mean(times)
+            if len(ti) != 0:
+                times.append(np.mean(ti))
+        return np.mean(times) if len(times) > 0 else 0.0
 
     def peace_metric(self):
         """
@@ -648,7 +649,7 @@ class CPRGridEnv(MultiAgentEnv, gym.Env):
                     for tagging_checkpoint in self.tagging_history
                 ]
             )
-        return (self.n_agents * self.elapsed_steps - total) / self.elapsed_steps
+        return (self.n_agents * self.elapsed_steps - total) / self.n_agents
 
     def get_social_outcome_metrics(self):
         """

src/policies.py
@@ -105,6 +105,7 @@ class VPGPolicy:
             # Accumulate trajectories to fill-up a batch of examples
             trajectories = memory.TrajectoryPool()
             epoch_infos = defaultdict(list)
+            epoch_episodes = 0
             while trajectories.get_timesteps() < steps_per_epoch:
                 logger.info(f"Episode {current_episode + 1}")
                 (
@@ -124,6 +125,7 @@ class VPGPolicy:
                     f"{np.mean(mean_returns[-episodes_mean_return:])}"
                 )
                 current_episode += 1
+                epoch_episodes += 1
 
             # Get a batch of (s, a, r) tuples
             actual_batch_size = trajectories.get_timesteps()
@@ -158,12 +160,19 @@ class VPGPolicy:
 
             # Compute mean epoch metrics
             if len(epoch_infos) > 0:
-                epoch_infos = {k: v / (epoch + 1) for k, v in epoch_infos.items()}
+                epoch_infos = {k: np.mean(v) for k, v in epoch_infos.items()}
                 logger.info(f"Epoch infos: {epoch_infos}")
 
             # Log to wandb at end of epoch
             if enable_wandb:
-                wandb_run.log({**epoch_infos, "loss": total_loss}, step=epoch)
+                wandb_run.log(
+                    {
+                        **epoch_infos,
+                        "loss": total_loss,
+                        "mean_return": np.mean(mean_returns[-epoch_episodes:]),
+                    },
+                    step=epoch,
+                )
 
             # Backprop
             optimizer.zero_grad()
@@ -226,7 +235,6 @@ class VPGPolicy:
 
         # Iterate as long as agents are not done or until
         # we reach the maximum number of time-steps
-        all_infos = defaultdict(list)
         for _ in range(self.max_steps):
             # Compute the best actions based on the current policy
             action_dict, action_probs = dict(), dict()
@@ -243,11 +251,6 @@ class VPGPolicy:
 
             # Perform a step in the environment
             new_observations, rewards, dones, infos = self.env.step(action_dict)
-
-            # Sum all agents' metrics
-            if "__all__" in infos:
-                for k, v in infos["__all__"].items():
-                    all_infos[k] += [v]
 
             # Update each agent's trajectory
             for agent_handle in range(self.n_agents):
@@ -267,14 +270,14 @@ class VPGPolicy:
                 logger.debug(f"Early stopping, all agents done")
                 break
 
-        # Compute a mean of episode infos
-        if "__all__" in infos:
-            all_infos = {k: np.mean(v) for k, v in all_infos.items()}
+        # Store episode infos
+        all_infos = infos["__all__"] if "__all__" in infos else dict()
 
         # Compute mean episode return
-        mean_return = np.mean(
-            [t.get_returns(discount=1, to_go=False) for t in trajectories]
-        )
+        agents_returns = [
+            t.get_returns(discount=1, to_go=False, as_torch=False) for t in trajectories
+        ]
+        mean_return = np.mean(agents_returns)
 
         return trajectories, all_infos, mean_return
 